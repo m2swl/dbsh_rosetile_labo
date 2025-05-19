e296689e651c6213f7db7e733fd931c123097c86
m2swl <180375327+m2swl@users.noreply.github.com>
2025-05-19 21:48:35 +0900

    maintenance

--- sw.js
+++ sw.js
@@ -2,15 +2,14 @@
 
 const CACHE_NAME = 'sensor-viewer-cache-v1';
 const urlsToCache = [
-  './', // index.html をキャッシュ (start_urlが'./'の場合)
+  './',
   './index.html',
   './manifest.json',
-  // CSSファイルや主要なJSファイル、アイコンなどもここに追加
   // './style.css',
   // './app.js',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
-  'https://unpkg.com/@material/web/all.js?module', // CDNもキャッシュ可能だが、更新に注意
+  'https://unpkg.com/@material/web/all.js?module', 
   'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap',
   'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
 ];
@@ -31,7 +30,6 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   console.log('[ServiceWorker] Activate');
-  // 古いキャッシュを削除する処理などをここに追加することが多い
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -52,7 +50,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // キャッシュがあればそれを返す (Cache First戦略)
         if (response) {
           return response;
         }
